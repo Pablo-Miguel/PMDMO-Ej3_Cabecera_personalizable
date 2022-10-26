77e8fa21a0b84ec8c2274123de73a719db68b103
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-10-26 14:32:44 +0200

    V1

---
+++ src/app/cabecera/cabecera.component.ts
@@ -0,0 +1,50 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.css']
+})
+export class CabeceraComponent implements OnInit {
+
+  direccion: String;
+  desaparecer: Boolean;
+  angular: Boolean;
+  js: Boolean;
+
+  constructor() { 
+    this.direccion = "assets/imgs/logo1.png" 
+    this.desaparecer = false;
+    this.angular = false;
+    this.js = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+  cambiarDirAngular() {
+    if(!this.angular){
+      this.direccion = "assets/imgs/logo2.png";
+    }
+    else {
+      this.direccion = "assets/imgs/logo1.png";
+    }
+    this.angular = !this.angular;
+  }
+
+  @ViewChild("imgjs") imgDOM!: ElementRef;
+
+  cambiarDirJs() {
+    if(!this.js){
+      this.imgDOM.nativeElement.src="assets/imgs/logo2.png";
+    }
+    else {
+      this.imgDOM.nativeElement.src="assets/imgs/logo1.png";
+    }
+    this.js = !this.js;
+  }
+
+  cambiarDesaparecer() {
+    this.desaparecer = !this.desaparecer;
+  }
+}
